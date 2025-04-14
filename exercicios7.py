0c4db5a38d19db065a2648dd96181976c9f42e66
Ana Santos <89166056+Anaclopes@users.noreply.github.com>
2025-04-14 08:20:52 -0300

    verificação exercicios7.py

--- exercicios7.py
+++ exercicios7.py
@@ -1,5 +1,9 @@
 Numero = int(input("Insira um número: "))
-for linha in range(1, Numero+1): #responsável por percorrer cada linha
-    for cada in range (linha): # para cada for, uma linha
-        print("x ", end=" ")
-    print(" ") # usei para pular as linhas
+
+if Numero <1:
+    print("Inválido")
+else:
+    for linha in range(1, Numero+1): #responsável por percorrer cada linha
+        for cada in range (linha): # para cada for, uma linha
+            print("x ", end=" ")
+        print(" ") # usei para pular as linhas
