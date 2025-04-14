02186c70cebd827a89957b6c8a2c937229af12de
Ana Santos <89166056+Anaclopes@users.noreply.github.com>
2025-04-14 08:11:18 -0300

    verificação exercicio4.py

--- exercicio4.py
+++ exercicio4.py
@@ -1,7 +1,14 @@
 numero = int(input("Insira o número que deseja calcular: "))
 i = 1
 nm = 1
-while i <= numero:
-    nm *= i
-    i+=1
+
+if numero > 0:
+    while i <= numero:
+        nm *= i
+        i+=1
+elif numero < 0:
+    print("INVÁLIDO")
+    
+elif numero == 0:
+    nm == 1
 print(nm)
