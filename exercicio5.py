2e3c15ca736a2e2cfbc6ca1676b346ef3f18f4ea
Ana Santos <89166056+Anaclopes@users.noreply.github.com>
2025-04-14 08:15:00 -0300

    verificação exercicio5.py

--- exercicio5.py
+++ exercicio5.py
@@ -2,10 +2,12 @@ num = int(input("Digite um número: "))
 divisor = 0
 
 for i in range(1, num +1):
-    if num % i == 0:
+    if num % i == 0 and num >= 1:
         divisor +=1
 
 if divisor == 2:
     print(f"{num} é primo")
+elif divisor <1:
+    print("Inválido")
 else:
     print(f"{num} não é primo")
