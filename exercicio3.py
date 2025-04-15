7bfe664023c57de6044dd035807192f9fadb5251
Ana Santos <89166056+Anaclopes@users.noreply.github.com>
2025-04-15 10:52:33 -0300

    Update exercicio3.py

--- exercicio3.py
+++ exercicio3.py
@@ -1,8 +1,5 @@
 numero = int(input("Insira um número: "))
 
-if numero < 0:
-    print("INVALIDO")
-else:
-    for numb in range(1,11):
-        mult = numero * numb
-        print(f"{numero} x {numb} = {mult}")
+for numb in range(1,11):
+    mult = numero * numb
+    print(f"{numero} x {numb} = {mult}")
