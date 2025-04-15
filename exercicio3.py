5afebf70eaa52069f4da2308c3f96a0f577a256a
Ana Santos <89166056+Anaclopes@users.noreply.github.com>
2025-04-15 10:46:12 -0300

    Update exercicio3.py

--- exercicio3.py
+++ exercicio3.py
@@ -4,5 +4,5 @@ if number < 0:
     print("INVALIDO")
 else:
     for numb in range(1,11):
-        mult= number*numb
-        print(f"{number} x {numb}:", mult)
+        mult = number * numb
+        print(f"{number} x {numb} = {mult}")
